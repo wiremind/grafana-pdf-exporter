9dd732b8b4f9c4a0b368e08620b720d9e13a91fe
Francois Minaud <fminaud@wiremind.io>
2021-06-21 14:12:15 +0200

    Do not copy exported pdf to AWS if AWS credentials not set

--- grafana_pdf_exporter.sh
+++ grafana_pdf_exporter.sh
@@ -4,4 +4,15 @@
 # Will copy the pdf file to an AWS bucket
 
 node grafana_pdf.js $1 $GRAFANA_USER:$GRAFANA_PASSWORD $2
-aws s3 cp $2 s3://$AWS_BUCKET_NAME/
+
+if [ ! -f $2 ]; then
+    echo "Grafana dashboard $1 not exported to $2, exiting"
+    exit 1
+fi
+
+### is AWS_REGION mandatory?
+if [[ ! -z $AWS_BUCKET_NAME && ! -z $AWS_ACCESS_KEY_ID && ! -z $AWS_SECRET_ACCESS_KEY && ! -z $AWS_REGION ]]; then
+  echo 'Copying $2 to s3://$AWS_BUCKET_NAME/'
+  aws s3 cp $2 s3://$AWS_BUCKET_NAME/
+  exit 0
+fi
